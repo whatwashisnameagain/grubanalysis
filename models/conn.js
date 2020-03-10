1d3ddb971f86e8a688e4a4369a8203ff401faa88
Reuben Remy <reubenremy@gmail.com>
2020-03-10 13:56:45 -0400

    Cleaned up project data

--- models/conn.js
+++ models/conn.js
@@ -6,23 +6,12 @@ const pgp = require('pg-promise')({
     }
 })
 
-
-// const options = {
-//     host: "drona.db.elephantsql.com",
-//     user: "ymofaetq",
-//     database: "ymofaetq",
-//     password: "KgRJJTBmhmV6lnOj4NmY-lXbiCsCILLS"
-// }
-
 const options = {
     host: process.env['DB_HOST'],
     user: process.env['DB_USER'],
     database: process.env['DB_NAME'],
     password: process.env['DB_PASSWORD']
 }
-
-// console.table(options, 'OPTIONS');
-console.table(process.env);
 
 const db = pgp(options);
 
