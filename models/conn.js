8deff67aae88d8c930c58bf335b13f91bba62a82
Reuben Remy <reubenremy@gmail.com>
2020-03-10 13:54:17 -0400

    Final environment files set

--- models/conn.js
+++ models/conn.js
@@ -7,17 +7,22 @@ const pgp = require('pg-promise')({
 })
 
 
+// const options = {
+//     host: "drona.db.elephantsql.com",
+//     user: "ymofaetq",
+//     database: "ymofaetq",
+//     password: "KgRJJTBmhmV6lnOj4NmY-lXbiCsCILLS"
+// }
+
 const options = {
-    host: "drona.db.elephantsql.com",
-    user: "ymofaetq",
-    database: "ymofaetq",
-    password: "KgRJJTBmhmV6lnOj4NmY-lXbiCsCILLS"
+    host: process.env['DB_HOST'],
+    user: process.env['DB_USER'],
+    database: process.env['DB_NAME'],
+    password: process.env['DB_PASSWORD']
 }
 
-// host: process.env['DB_HOST'],
-// user: process.env['DB_USER'],
-// database: process.env['DB_NAME'],
-// password: process.env['DB_PASSWORD']
+// console.table(options, 'OPTIONS');
+console.table(process.env);
 
 const db = pgp(options);
 
